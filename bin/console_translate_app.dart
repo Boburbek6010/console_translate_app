9a25ac978efc7198ac9e8697f846d417ceca202c
Boburbek6010 <boburbekkhusanboev@gmail.com>
2023-11-27 12:01:54 +0500

    first commit

---
+++ bin/console_translate_app.dart
@@ -0,0 +1,5 @@
+import 'package:console_translate_app/console_translate_app.dart' as console_translate_app;
+
+void main(List<String> arguments) {
+  print('Hello world: ${console_translate_app.calculate()}!');
+}
---
+++ test/console_translate_app_test.dart
@@ -0,0 +1,8 @@
+import 'package:console_translate_app/console_translate_app.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
