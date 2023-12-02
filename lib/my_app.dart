9317780c9ba8458830f011043e9f7d75a2ea0d81
muslima <muslima241105@gmail.com>
2023-12-02 11:32:42 +0500

    Muslima, 3-commit, commit to pull

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,9 +1,5 @@
-
-import 'package:console_translate_app/menus/home_menu.dart';
-import 'package:console_translate_app/menus/main_menu.dart';
 import 'package:console_translate_app/services/extension_service.dart';
 import 'package:console_translate_app/services/language_service.dart';
-import 'package:console_translate_app/services/ui_services.dart';
 
 void main() {
   // while(true){
@@ -13,9 +9,9 @@ void main() {
 
   // addNewWord();
   // translateWord();
-
-  print("welcome".tr);
-  LanguageService.switchLanguage = "1";
+  //
+  // print("welcome".tr);
+  // LanguageService.switchLanguage = "1";
 
 }
 
