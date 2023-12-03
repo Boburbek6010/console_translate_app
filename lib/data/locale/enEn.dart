a326e872e845b7aff81d87c50566e79b64ac7854
muslima <muslima241105@gmail.com>
2023-12-03 12:40:46 +0500

    Muslima, 6-commit, commit to push translate functions and history functions

--- lib/data/locale/enEn.dart
+++ lib/data/locale/enEn.dart
@@ -2,5 +2,9 @@ const Map<String, String> en = {
   "welcome":"Welcome to our app\n",
   "setting":"Setting menu\n",
   "exit":"Exit\n",
-  "start":"Start\n"
+  "start":"Start\n",
+  "askUsername":"Please enter your username:\n ",
+  "askPhoneNumber":"Please enter you phone number\nNote: It should be 9 digits\n",
+  "askPassword":"Please enter your password:\n",
+  "successfull":"Successfully registered!\n",
 };
