f8a83ff965493db84c0f1705350ef4fbb4b5583f
muslima <muslima241105@gmail.com>
2023-12-05 09:25:06 +0500

    Muslima, 9-commit, check if the entered language contains the language list

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,9 +1,5 @@
-
-import 'package:console_translate_app/menus/check_with_ai.dart';
 import 'package:console_translate_app/menus/dictionary_menu.dart';
 import 'package:console_translate_app/services/language_service.dart';
-
-import 'menus/addNewWord_menu.dart';
 import 'menus/main_menu.dart';
 
 void main() {
